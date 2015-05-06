f1b416fc5e7185a68daf9b1b42ddaa5d1eb846ba
mayanj <mayanj@MAA-D322HY1.ant.amazon.com>
2015-05-06 14:33:32 +0530

    some

---
+++ InterviewBasics/src/my/projects/arrays/RotateAnArrayBYK.java
@@ -0,0 +1,44 @@
+package my.projects.arrays;
+
+public class RotateAnArrayBYK {
+
+	public static void main(String args[]) {
+		RotateAnArrayBYK anArrayBYK = new RotateAnArrayBYK();
+
+		int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		anArrayBYK.printArray(array);
+		anArrayBYK.rotate(3, array, anArrayBYK);
+		anArrayBYK.printArray(array);
+		// anArrayBYK.rotate(4, array);
+		// anArrayBYK.printArray(array);
+	}
+
+	private void rotate(int k, int[] array, RotateAnArrayBYK anArrayBYK) {
+		// First rotate the whole Array;
+		reverse(array, 0, array.length);
+		reverse(array, 0, k);
+		reverse(array, k, array.length - k);
+
+	}
+
+	private void reverse(int array[], int start, int length) {
+
+		if (start == length / 2) {
+			length = length + 1;
+		}
+		for (int j = start; j < length / 2; j++) {
+			int temp = array[j];
+			array[j] = array[(length - j) - 1];
+			array[(length - j) - 1] = temp;
+
+		}
+	}
+
+	private void printArray(int array[]) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
---
+++ InterviewBasics/src/my/projects/series/FibbonicciSeries.java
@@ -0,0 +1,62 @@
+package my.projects.series;
+
+public class FibbonicciSeries {
+
+	// 1,1,2,3,5,8,11
+
+	public static void main(String arhs[]) {
+
+		FibbonicciSeries fibbonicciSeries = new FibbonicciSeries();
+		fibbonicciSeries.fibbo(10);
+	}
+
+	private void fibbo(int n) {
+
+		for (int i = 1; i <= n; i++) {
+			System.out.print(fibonacciRecusion(i) + " ");
+		}
+
+		System.out.println();
+
+		// for (int i = 1; i <= n; i++) {
+		fibonacciLoop(n);
+		// }
+
+		System.out.println(fib(10));
+
+	}
+
+	private int fibonacciRecusion(int n) {
+		if (n == 1 || n == 2) {
+			return 1;
+		}
+
+		return fibonacciRecusion(n - 1) + fibonacciRecusion(n - 2);
+	}
+
+	public static void fibonacciLoop(int number) {
+		int fibo1 = 1, fibo2 = 1, fibonacci = 1;
+		System.out.print(fibo1 + " " + fibo2 + " ");
+		for (int i = 3; i <= number; i++) {
+			fibonacci = fibo1 + fibo2;
+			fibo1 = fibo2;
+			fibo2 = fibonacci;
+			System.out.print(fibonacci + " ");
+
+		}
+
+	}
+
+	private double fib(int n) {
+		double root5 = Math.sqrt(5);
+		double gr = (1 + root5) / 2;
+		double igr = 1 - gr;
+		double value = (Math.pow(gr, n) - Math.pow(igr, n)) / root5;
+
+		// round it to the closest integer since floating
+		// point arithmetic cannot be trusted to give
+		// perfect integer answers.
+		return Math.floor(value + 0.5);
+	}
+
+}
